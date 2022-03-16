d652a58aebb6dcbe585b1fdabe6773caddf88780
rizkaayu123 <90969157+rizkaayu123@users.noreply.github.com>
2022-03-16 23:39:38 +0700

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: ['plugin:vue/essential', '@vue/airbnb'],
+    parserOptions: {
+      parser: 'babel-eslint',
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      // disable error when importing without '.ext'
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          vue: 'never',
+        },
+      ],
+    },
+    // create override to handle multiple os linebreak system
+    overrides: [
+      {
+        // specify files to override
+        files: ['*.vue', '*.js'],
+        rules: {
+          // override rules
+          // 'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+          'linebreak-style': 0,
+        },
+      },
+    ],
+  };
+  
